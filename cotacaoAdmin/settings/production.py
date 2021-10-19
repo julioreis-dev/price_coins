e09ad1f216be5ceaaf632c1d6f5983cd21e0a689
Julio dos Reis Firmino <julreifir@yahoo.com.br>
2021-10-19 12:02:09 -0300

    fix(settings/production): fix allowed_hosts
    fix the address in production.py in allowed_hosts

--- cotacaoAdmin/settings/production.py
+++ cotacaoAdmin/settings/production.py
@@ -1,12 +1,11 @@
 from cotacaoAdmin.settings.settings import *
 
-
-DEBUG = False
+DEBUG = True
 
 SECRET_KEY = 'django-insecure-r05@$*8=pv@gg0yht@+lhbn8ifiqotd1_u29mb-f3kez(f@moe'
 
 # Alterar para o IP do ambiente de produção quando houver.
-ALLOWED_HOSTS = ['https://challengebr.herokuapp.com/']
+ALLOWED_HOSTS = ['challengebr.herokuapp.com']
 
 DATABASES = {
     'default': {
