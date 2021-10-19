61f1e49358f41b9a49259e7dbf80632f3f6783a8
Julio dos Reis Firmino <julreifir@yahoo.com.br>
2021-10-19 11:22:10 -0300

    feat(settings/production): included production.py
    included production.py in the project

---
+++ cotacaoAdmin/settings/production.py
@@ -0,0 +1,16 @@
+from cotacaoAdmin.settings.settings import *
+
+
+DEBUG = False
+
+SECRET_KEY = 'django-insecure-r05@$*8=pv@gg0yht@+lhbn8ifiqotd1_u29mb-f3kez(f@moe'
+
+# Alterar para o IP do ambiente de produção quando houver.
+ALLOWED_HOSTS = ['https://challengebr.herokuapp.com/']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR / 'db.sqlite3'),
+    }
+}
