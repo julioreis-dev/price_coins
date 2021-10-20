211473f6de0d68dfd7c7132009825245044c23d2
Julio dos Reis Firmino <julreifir@yahoo.com.br>
2021-10-20 11:55:22 -0300

    feat(views/cotacao): removed class information
    the class information was removed of inside cotacao.py

--- cotacao/views/cotacao.py
+++ cotacao/views/cotacao.py
@@ -1,51 +1,9 @@
-from dataclasses import dataclass
 from django.shortcuts import render
 from cotacao.forms.cotacao import CotacoesForm
-from cotacao.actions.controlers import read_api
-from cotacao.models.cotacaomodel import Cotacoes
 from cotacao.actions.chart import dataview
+from cotacao.actions.controler_bd import Information
 from datetime import timedelta, datetime
 from multiprocessing.dummy import Pool as ThreadPool
-
-
-@dataclass
-class Information:
-    """
-    Constructor for Information
-    :param contents: A list with date, coin, values.
-    """
-    contents: list = None
-
-    @staticmethod
-    def searchdatabd(data: str) -> bool:
-        """
-        :param data: A current date.
-        :return: boolean
-        """
-        result = Cotacoes.objects.filter(date=data).first()
-        return False if result is None else True
-
-    def insertbd(self):
-        """
-        Insert all information into database with bulk_create method.
-        :return: None
-        """
-        list_instance = []
-        for content in self.contents:
-            document = Cotacoes(date=content[0], coin=content[1], values=content[2])
-            list_instance.append(document)
-        Cotacoes.objects.bulk_create(list_instance)
-
-    def transaction(self, atual: str) -> None:
-        """
-        :param atual: date to be insert in database
-        :return: None
-        """
-        cond = self.searchdatabd(atual)
-        if not cond:
-            result = read_api(atual)
-            infobd = Information(list(result))
-            infobd.insertbd()
 
 
 def upload_cotation(request):
