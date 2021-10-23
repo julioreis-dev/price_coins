5745b78d0addb7116071a2f65ff2537bdae39b88
Julio dos Reis Firmino <julreifir@yahoo.com.br>
2021-10-23 10:26:31 -0300

    feat(actions/chart): add django_filters
    Included django_filters in the file settings.py in installed_apps

--- cotacaoapi/views.py
+++ cotacaoapi/views.py
@@ -1,10 +1,10 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.views import APIView
 from cotacao.models.cotacaomodel import Cotacoes
 from .serializers import CotacaoSerializer
 from rest_framework.response import Response
-from rest_framework import status
 from django.http import Http404
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class CotacaoViewSet(viewsets.ReadOnlyModelViewSet):
@@ -55,20 +55,26 @@ class DateViewSet(APIView):
         # return Response(serializer.data)
 
 
-class MixViewSet(APIView):
-    """
-    This class return all register in database by coin and date
-    :return:json
-    """
+# class MixViewSet(APIView):
+#     """
+#     This class return all register in database by coin and date
+#     :return:json
+#     """
+#     serializer_class = CotacaoSerializer
+#
+#     def get_object(self, name, date):
+#         try:
+#             return Cotacoes.objects.filter(coin=name, date=date).all()
+#         except Cotacoes.DoesNotExist:
+#             raise Http404
+#
+#     def get(self, request, name, date, format=None):
+#         mix = self.get_object(name, date)
+#         serializer = CotacaoSerializer(mix, many=True)
+#         return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
+
+class MixViewSet(viewsets.ModelViewSet):
+    queryset = Cotacoes.objects.all()
     serializer_class = CotacaoSerializer
-
-    def get_object(self, name, date):
-        try:
-            return Cotacoes.objects.filter(coin=name, date=date).all()
-        except Cotacoes.DoesNotExist:
-            raise Http404
-
-    def get(self, request, name, date, format=None):
-        mix = self.get_object(name, date)
-        serializer = CotacaoSerializer(mix, many=True)
-        return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['coin', 'date']
