6151ed68849b490e85070e974623f4e760e8deea
Julio dos Reis Firmino <julreifir@yahoo.com.br>
2021-11-26 12:52:01 -0300

    feat(cotacao/models): Improvements in cotacaomodel.py

--- cotacao/models/cotacaomodel.py
+++ cotacao/models/cotacaomodel.py
@@ -5,7 +5,6 @@ from django.utils import timezone
 class Cotacoes(models.Model):
     date = models.DateField(default=timezone.now())
     coin = models.CharField(max_length=50, blank=True, null=True)
-    # coin = models.CharField(choices=COIN_CHOICE, max_length=50, default='Real')
     values = models.FloatField(default=0)
 
     def __str__(self):
